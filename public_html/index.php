59b4f4a111b23d625aeda6b7f90426659d4824b3
Diak <Diak@T018CG13TEACH>
2018-01-15 17:01:08 +0100

    minden kész

--- public_html/index.php
+++ public_html/index.php
@@ -1,23 +1,28 @@
+<?php
+$skin="light";
+    if(isset($_GET['skin']) and $_GET['skin']=="light")
+    {
+        $skin="light";
+    }
+ else if(isset($_GET['skin']) and $_GET['skin']=="dark")
+    {
+        $skin="dark";
+    }
+?>
 <!DOCTYPE html>
+
 <html>
     <head>
         <title>Dark web</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="light.css">
-        <link rel="stylesheet" type="text/css" href="dark.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo $skin; ?>.css">
     </head>
     <body>
         <div><h1>Világos?!</h1></div>
         <div><p>Ez egy dark üzemmódot támogató oldal!</p></div>
-        <form method="GET">
-            <input type="hidden" name="css" value="vilagos">
-            <input type="submit" value="Világos">
-        </form>
-        <form method="GET">
-            <input type="hidden" name="css" value="sotet">
-            <input type="submit" value="Sötét">
-        </form>
+        <a href="?skin=light" >világos</a>
+        <a href="?skin=dark" >sötét</a>
     </body>
 </html>
 
